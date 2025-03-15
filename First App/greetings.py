63392adca7a7f50ae4b6b16b67014ebeaf808f0a
Marlonn Locatelli <marlonn@gmail.com>
2025-03-15 17:43:13 -0300

    frames: using

--- First App/greetings.py
+++ First App/greetings.py
@@ -3,24 +3,36 @@ from tkinter import ttk
 
 
 def greet():
-    print(f"Hello, {user_name.get() or 'World'}!")
+    print(f"Hello, {user_name.get().title() or 'World'}!")
 
 
 root = tk.Tk()
+root.title("Greeter")
 
 user_name = tk.StringVar()
 
-name_label = ttk.Label(root, text="Name:")
+
+# Input
+input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
+input_frame.pack(fill="both")
+
+name_label = ttk.Label(input_frame, text="Name:")
 name_label.pack(side="left", padx=(0, 10))
 
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)
-name_entry.pack(side="left")
+name_entry = ttk.Entry(input_frame, width=15, textvariable=user_name)
+name_entry.pack(side="left", fill="x", expand=True)
 name_entry.focus()
 
-greet_button = ttk.Button(root, text="Greet", command=greet)
+
+# Buttons
+
+buttons_frame = ttk.Frame(root, padding=(20, 10))
+buttons_frame.pack(fill="both")
+
+greet_button = ttk.Button(buttons_frame, text="Greet", command=greet)
 greet_button.pack(side="left", fill="x", expand=True)
 
-quit_button = ttk.Button(root, text="Quite", command=root.destroy)
+quit_button = ttk.Button(buttons_frame, text="Quit", command=root.destroy)
 quit_button.pack(side="left", fill="x", expand=True)
 
 root.mainloop()
