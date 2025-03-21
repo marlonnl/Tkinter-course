c9d43b46f3f137b5e309df11569f4f4ee5e07da3
Marlonn Locatelli <marlonn@gmail.com>
2025-03-21 17:26:33 -0300

    oop: frame class

---
+++ OOP tkinter/framing_oop.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+class UserInputFrame(ttk.Frame):
+    "Framing tkinter widgets in classes"
+
+    def __init__(self, container):
+        super().__init__(container, padding=(15))
+
+        self.user_input = tk.StringVar()
+
+        label = ttk.Label(self, text="Enter your name:")
+        label.pack()
+
+        entry = ttk.Entry(self, textvariable=self.user_input)
+        entry.focus()
+        entry.pack()
+
+        button = ttk.Button(self, text="Greet!", command=self.greet)
+        button.pack()
+
+        # Binds
+        container.bind("<Return>", self.greet)
+        container.bind("<KP_Enter>", self.greet)
+        container.bind("<Escape>", lambda _: container.destroy())
+
+    def greet(self, *args):
+        print(f"Hello, {self.user_input.get()}!")
+
+
+root = tk.Tk()
+
+my_frame = UserInputFrame(root)
+my_frame.pack()
+
+
+root.mainloop()
